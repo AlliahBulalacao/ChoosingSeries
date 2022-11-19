87008f2b85dc2d0d9a7542237c73f0f317d21c45
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2022-11-19 16:36:48 +0800

    Input Option of the User

--- ChoosingSeries.py
+++ ChoosingSeries.py
@@ -1,1 +1,13 @@
 print("This is my interesting program about choosing a series")
+
+# Being indecisive is hard. Since, I love watching series but still, having a hard time choosing what to watch.
+# I'm lucky to got passed by in one of "Tina Huang" youtube video. So, I have some idea on how to do a program
+# in choosing a random series.
+# Here's the link on her video, (https://youtu.be/_xf1TMs0ysk)
+
+from random import choice
+
+# Letting the user choose what he wants to watch, be it anime, series, movie.
+option = input("What do you want to watch?\n(Anime/Series/Movie): ")
+
+
