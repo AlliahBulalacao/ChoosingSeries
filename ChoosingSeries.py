d19bd1585f946750ebb18a99f841db4d244890a5
AlliahBulalacao <116430363+AlliahBulalacao@users.noreply.github.com>
2022-11-19 20:56:52 +0800

    Adding option

--- ChoosingSeries.py
+++ ChoosingSeries.py
@@ -1,4 +1,4 @@
-print("This is my interesting program about choosing a series")
+print("This program allows you to choose what kind of movie, series or anime you want to watch. ")
 
 # Being indecisive is hard. Since, I love watching series but still, having a hard time choosing what to watch.
 # I'm lucky to got passed by in one of "Tina Huang" youtube video. So, I have some idea on how to do a program
@@ -16,34 +16,37 @@ while True:
     option = input("What do you want to watch? ")
     # create a list for anime
     anime = ['Your Name', 'Haikyuu', 'Black Clover', 'One Piece', 'Kimi Ni Todoke', 'Jujutsu Kaisen',
-             'Seven Deadly Sins']
+             'Seven Deadly Sins', 'Demon Slayer', 'Spy x Family', 'My Hero Academia', 'Ride your wave', 'Kurokos Basketball',
+             'Naruto', 'HunterxHunter', 'Yowamushi Pedal', 'Doraemon']
     # create a list for series option
     series = ['Gilmore Girls', 'Greys Anatomy', 'Stranger Things', 'Sex Education', 'The Umbrella Academy',
-              'Bridgerton', 'Queens Gambit', 'Euphoria', 'Money Heist', 'Emily in Paris']
+              'Bridgerton', 'Queens Gambit', 'Euphoria', 'Money Heist', 'Emily in Paris', ]
     # create a list for movie option
     hw_movie = ['10 things I hate about you', 'Enola Homes', 'Wonder', 'Mean Girls', 'The Devil Wears Prada',
                 'Breakfast Club', 'Flipped', 'Narnia', 'Bridge to Terabithia', 'Poltergeist', 'Insidious', 'The Notebook',
-                ]
+                '']
     filo_movie = ['Barcelona: A love untold', 'The Hows of Us', 'My Ex and Whys', 'Im Drunk, I love you',
                   'Kita Kita', 'Doll House', 'Goyo: Ang Batang Heneral', 'Heneral Luna', 'Four Sisters and A Wedding',
-                  'Seven Sundays']
+                  'Seven Sundays', 'Four sisters and a Wedding', 'Shes Dating the Gangster', 'Isang Daan Tula Para kay Stella',
+                  'The Gifted', 'Unforgettable', 'Miss Granny']
     k_movie = ['On your Wedding Day', '20th Century Girl', 'Sweet and Sour', 'Seoul Vibe', 'Peninsula', 'Parasite',
-               'Train to Busan', 'Alive', 'The Call']
+               'Train to Busan', 'Alive', 'The Call', '200 pound beauty']
+    others =['Three Idiots', 'PK', 'Taarezameenpar', 'Crazy little thing called love', 'Friend zone']
     if option == '1':
-        print(choice(anime))
+        print("This is a recommended anime for you:",(choice(anime)))
     elif option  == '2':
-        print(choice(series))
+        print("This is a recommended series for you:", (choice(series)))
     elif option =='3':
-        for_movie = input("What kind of movie do you want to watch?\n"
+        for_movie = input("\nWhat kind of movie do you want to watch?\n"
                           "1. Hollywood Movies\n"
                           "2. Filipino Movies\n"
                           "3. Korean Movies\n"
                           "4. Others\n"
                           "Answer: ")
         if for_movie == '1':
-            print(choice(hw_movie))
+            print("This is a recommended Hollywood movie for you:", (choice(hw_movie)))
         elif for_movie == '2':
-            print(choice(filo_movie))
+            print("This is a recommended Filipino movie for you:", (choice(filo_movie)))
         elif for_movie == '3':
             print(choice(k_movie))
     else:
